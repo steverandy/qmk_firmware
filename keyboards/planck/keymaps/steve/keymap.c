9e615fdcdf205b2e1e7cc466b528ad7da4d860c0
Steve Randy Tantra <steve.randy@gmail.com>
2016-08-16 13:34:19 +0800

    Update layer numbers

--- keyboards/planck/keymaps/steve/keymap.c
+++ keyboards/planck/keymaps/steve/keymap.c
@@ -15,11 +15,11 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 2
-#define _RAISE 3
-#define _FUNCTION 4
-#define _NUMERIC 5
-#define _PLOVER 6
+#define _LOWER 1
+#define _RAISE 2
+#define _FUNCTION 3
+#define _NUMERIC 4
+#define _PLOVER 5
 #define _ADJUST 16
 
 enum planck_keycodes {
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {MO(4),   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {MO(3),   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
